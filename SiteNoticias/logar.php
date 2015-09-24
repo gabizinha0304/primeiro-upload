2d7481016af73d762f06dbe02454b327ab0a2d6a
C1PH3R <gu.bat98@gmail.com>
2015-09-24 18:53:42 -0300

    Update logar.php
    Mudei a conexao, e o if.

--- SiteNoticias/logar.php
+++ SiteNoticias/logar.php
@@ -1,36 +1,30 @@
-<?php
-  $conexao = mysql_connect("localhost","root","");
-		     mysql_select_db("sitenoticia",$conexao);
-			 
-  	  	$SQL1="select * from logar order by login";
-	    $resultado1= mysql_query($SQL1,$conexao);
-		
-	    $SQL2="select * from logar order by senha";
-	    $resultado2= mysql_query($SQL2,$conexao);
-		
-  session_start();
-  
+<?php  
   $usuario= $_POST["usuario"];
   $senha= $_POST["senha"];
-  
-  if($usuario == "" && $senha == "")
-  {
-	  echo "<div>";
-	  echo "<p>Insira um usuario e senha</p>";
-	  echo "</div>";
-  }
-  else
-  {
+  //Configuraçoes da Conexão
+  	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "sitenoticia";
+	
+  //Conexao		
+  $conn = new mysqli($servername, $username, $password,$db);
+	    $SQL="select * from logar where login='$usuario'";
+	    $result = $conn->query($SQL);
+	    $dados = $result->fetch_assoc();
+    
+		
+ 
 
-		if($resultado1 == $usuario && $resultado2 == $senha)
-		{
-			header("Location:inicioAdm.php");
-		}
-		else
-		{
-		   echo "<div>";
-	       echo "<p>Usuario ou senha incorretos</p>";
-	       echo "</div>";
-		}
+ if($senha == $dados["senha"])
+	{
+		echo "Teste";
+	}
+	else
+	{
+		echo "Usuário ou senha incorretos!";
+		echo "<br>
+				<a href='login.html'>Voltar</a>";
+	}
   }
 ?>
